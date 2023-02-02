a305c63f502869edd5b634db59a96135198c2c72
heritier24 <heritier.ganza@kudibooks.com>
2023-02-02 23:49:10 +0200

    model of family members and migrations created

---
+++ database/migrations/2023_02_02_065446_create_family_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('family_members', function (Blueprint $table) {
+            $table->id();
+            $table->string("names");
+            $table->string("gender");
+            $table->string("phonenumber");
+            $table->string("marital_status");  // single , married
+            $table->integer("national_id")->nullable();
+            $table->string("status"); // active, inactive
+            $table->integer("person_id")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('family_members');
+    }
+};
