3595a47f993da507c948a7ffac663728ab42a3d3
heritier24 <heritier.ganza@kudibooks.com>
2023-02-02 23:32:33 +0200

    authentication endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('authentication')->group(function () {
+
+    Route::post('login', [AuthenticationController::class, 'login']);
+
+    Route::get('forgot-password', [AuthenticationController::class, 'forgotPassword']);
+
+    Route::put('reset-password', [AuthenticationController::class, 'resetPassword']);
+
+    Route::get('logout', [AuthenticationController::class, 'logout']);
 });
