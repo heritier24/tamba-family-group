700f8a7aa7189c7b6ed9bbe64f5b04575ce3ceb3
heritier24 <heritier.ganza@kudibooks.com>
2023-02-05 17:05:28 +0200

    generatl transaction model and migrations created well and it's where all transactions related to amount transactions savings, withdraw will be stored

---
+++ database/migrations/2023_02_05_104654_create_general_transactions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('general_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string("transaction_date");
+            $table->string("transaction_type"); // savings type, loan transaction type, interest rate type and , .....
+            $table->string("transaction_from"); // from bank, from ccash , from family members, ... 
+            $table->string("transaction_to");
+            $table->string("transaction_amount");
+            $table->unsignedBigInteger("user_id");
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('general_transactions');
+    }
+};
