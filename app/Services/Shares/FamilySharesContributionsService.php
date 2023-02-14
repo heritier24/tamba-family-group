e4ebb5207e3ca969a0584663b94d33fb426285f6
heritier24 <heritier.ganza@kudibooks.com>
2023-02-14 18:08:51 +0200

    update family contribution servie method implemented

--- app/Services/Shares/FamilySharesContributionsService.php
+++ app/Services/Shares/FamilySharesContributionsService.php
@@ -3,9 +3,12 @@
 namespace App\Services\Shares;
 
 use App\Exceptions\ItemNotFoundException;
+use App\Models\Account;
 use App\Models\FamilyHouseMembers;
 use App\Models\FamilyShareContribution;
 use App\Models\FamilyShareType;
+use App\Models\GeneralTransactions;
+use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -25,21 +28,20 @@ class FamilySharesContributionsService
         int $shareTypeID,
         int $houseMemberID,
         string $shareAmount,
-        string $amountPaid,
-        string $status
+        string $amountPaid
     ) {
         $this->validateShareTypeId($shareTypeID);
 
         $this->validateHouseMemberID($houseMemberID);
 
-        $createContribution = DB::transaction(function () use ($shareTypeID, $houseMemberID, $shareAmount, $amountPaid, $status) {
+        $createContribution = DB::transaction(function () use ($shareTypeID, $houseMemberID, $shareAmount, $amountPaid) {
 
             FamilyShareContribution::create([
                 "share_type_id" => $shareTypeID,
                 "house_member_id" => $houseMemberID,
                 "share_amount" => $shareAmount,
                 "amount_paid" => $amountPaid,
-                "status" => $status,
+                "status" => "Unpaid",
                 "user_id" => 1
             ]);
         });
@@ -70,8 +72,70 @@ class FamilySharesContributionsService
         return $familyContributions;
     }
 
-    public function updateFamilySharesContributions()
+    public function updateFamilySharesContributions(int $contributionID, string $amountPaid, string $status, $userID, $transactionTo)
     {
+        $updateCotributions = DB::transaction(function () use ($contributionID, $amountPaid, $status, $userID, $transactionTo) {
+            FamilyShareContribution::where("id", $contributionID)->update([
+                "amount_paid" => $amountPaid,
+                "status" => $status,
+                "user_id" => $userID
+            ]);
+            $transactionType = $this->findTransactionTypeToCreateTransaction($contributionID);
+
+            $transactionFrom = $this->findTransactionFromToCreateTransaction($contributionID);
+
+            GeneralTransactions::create([
+                "transaction_date" => now(),
+                "transaction_type" => $transactionType,
+                "transaction_from" => $transactionFrom,
+                "transaction_to" => $transactionTo,
+                "transaction_amount" => $amountPaid,
+                "user_id" => $userID
+            ]);
+            $account = Account::where("account_type", $transactionTo)->first();
+            if (!$account) {
+                Account::create([
+                    "account_type" => $transactionTo,
+                    "account_amount" => $amountPaid,
+                ]);
+            } else {
+                $accountId = $account->id;
+                $amount = $account->account_amount;
+                $accountAmount = $amount + $amountPaid;
+                Account::where("id", $accountId)->update([
+                    "account_amount" => $accountAmount
+                ]);
+            }
+        });
+
+        return $updateCotributions;
+    }
+
+    private function findTransactionTypeToCreateTransaction(int $contributionID): string
+    {
+        $contribution = DB::selectOne("SELECT family_share_types.share_type
+                                       FROM family_share_contributions 
+                                       INNER JOIN family_share_types 
+                                       ON family_share_contributions.share_type_id = family_share_types.id
+                                       WHERE family_share_contributions.id = ?", [$contributionID]);
+
+        if (!$contribution) {
+            throw new Exception("Transaction type not found");
+        }
+
+        return $contribution->share_type;
+    }
+
+    private function findTransactionFromToCreateTransaction(int $contributionID): string
+    {
+        $transactionFrom = DB::selectOne("SELECT family_members.names
+                                          FROM family_share_contributions
+                                          INNER JOIN family_house_members
+                                          ON family_share_contributions.house_member_id = family_house_members.id
+                                          INNER JOIN family_members
+                                          ON family_house_members.member_id = family_members.id");
+
+        return $transactionFrom->names;
     }
 
     private function validateShareTypeId(int $shareTypeID)
