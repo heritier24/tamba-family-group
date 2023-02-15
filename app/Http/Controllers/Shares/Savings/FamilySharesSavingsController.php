5e20f78dc5a1576351603aa250c85db8004af647
heritier24 <heritier.ganza@kudibooks.com>
2023-02-15 21:14:16 +0200

    update saving method setting created successfully

---
+++ app/Http/Controllers/Shares/Savings/FamilySharesSavingsController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Shares\Savings;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Savings\CreateShareSavingsRequest;
+use App\Http\Requests\Savings\UpdateShareSavingsRequest;
+use App\Services\Savings\FamilyShareSavingsService;
+
+class FamilySharesSavingsController extends Controller
+{
+    public function __construct(protected FamilyShareSavingsService $service)
+    {
+    }
+    public function listSavingsShare()
+    {
+        try {
+            $result = $this->service->listSavingsShare();
+
+            return response()->json($result);
+        } catch (\Exception $e) {
+            [$message, $statusCode, $exceptionCode] = getHttpMessageAndStatusCodeFromException($e);
+
+            return response()->json([
+                "message" => $message,
+            ], $statusCode);
+        }
+    }
+
+    public function recordSavings(CreateShareSavingsRequest $request)
+    {
+        try {
+            $this->service->recordSavings(
+                $request->share_type_id,
+                $request->house_member_id,
+                $request->saving_amount,
+                $request->number_of_shares,
+                $request->total_shares_amount,
+                $request->user_id
+            );
+
+            return response()->json(["Record Savings Share Created successfully"]);
+        } catch (\Exception $e) {
+            [$message, $statusCode, $exceptionCode] = getHttpMessageAndStatusCodeFromException($e);
+
+            return response()->json([
+                "message" => $message,
+            ], $statusCode);
+        }
+    }
+
+    public function updateSavings(UpdateShareSavingsRequest $request, int $savingID)
+    {
+        try {
+            $this->service->updateSavings(
+                $savingID,
+                $request->number_of_shares,
+                $request->total_shares_amount,
+                $request->status,
+                $request->user_id
+            );
+
+            return response()->json(["Share Savings Updated Successfully"]);
+        } catch (\Exception $e) {
+            [$message, $statusCode, $exceptionCode] = getHttpMessageAndStatusCodeFromException($e);
+
+            return response()->json([
+                "message" => $message,
+            ], $statusCode);
+        }
+    }
+
+    public function savingsTransactions()
+    {
+        try {
+            $this->service->savingsTransactions();
+
+            return response()->json(["Savings Transactions Saved Successfully"]);
+        } catch (\Exception $e) {
+            [$message, $statusCode, $exceptionCode] = getHttpMessageAndStatusCodeFromException($e);
+
+            return response()->json([
+                "message" => $message,
+            ], $statusCode);
+        }
+    }
+}
