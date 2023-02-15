fa21beb3207c7fc639ce2106f5f6469d6268ac3a
heritier24 <heritier.ganza@kudibooks.com>
2023-02-15 21:11:42 +0200

    create share saving and update share saving input form with validation done

---
+++ app/Http/Requests/Savings/CreateShareSavingsRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Savings;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class CreateShareSavingsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "share_type_id" => "required|integer",
+            "house_member_id" => "required|integer",
+            "saving_amount" => "required|string",
+            "number_of_shares" => "required|integer",
+            "total_shares_amount" => "required|integer",
+            "user_id" => "required|integer",
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(response()
+                ->json(
+                    [
+                        "errors" => $validator->errors()->all()
+                    ],
+                    Response::HTTP_UNPROCESSABLE_ENTITY
+                ));
+        }
+    }
+}
---
+++ app/Http/Requests/Savings/UpdateShareSavingsRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests\Savings;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class UpdateShareSavingsRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "number_of_shares" => "required|integer",
+            "total_shares_amount" => "required|integer",
+            "status" => "required|string",
+            "user_id" => "required|integer",
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        if ($validator->fails()) {
+            throw new HttpResponseException(response()
+                ->json(
+                    [
+                        "errors" => $validator->errors()->all()
+                    ],
+                    Response::HTTP_UNPROCESSABLE_ENTITY
+                ));
+        }
+    }
+}
