62eaad5ab372eb01e5b1ab14f271e1577849f348
heritier24 <heritier.ganza@kudibooks.com>
2023-02-17 08:42:09 +0200

    share saving transactions migration * amount tobe paid must be the of total share amount from share savings

--- database/migrations/2023_02_02_185254_create_family_share_saving_transactions_table.php
+++ database/migrations/2023_02_02_185254_create_family_share_saving_transactions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("family_share_saving_id"); // id of the family share saving foreign key
             $table->string("monthly_transaction"); // (January-2023, February-2023, March-2023, April-2023, ...)
-            $table->integer("amount_tobe_paid");
+            $table->integer("amount_tobe_paid");  // this is total share amount from family share savings
             $table->integer("amount_paid");
             $table->integer("remaining_amount");
             $table->string("status");  // unpaid, process , paid
